446443e12a87f9fb77bed0f43b829355426b73c0
Lia Harrington <lia.x.harrington.gr@dartmouth.edu>
2017-04-13 11:11:20 -0400

    updated for R assignment convention

--- Scripts/convert_gene_id.R
+++ Scripts/convert_gene_id.R
@@ -15,26 +15,26 @@
 
 library(org.Hs.eg.db)
 
-genes_df = read.csv('hgsc_cluster_genes.tsv', sep = '\t', row.names = 1)
+genes_df <- read.csv('hgsc_cluster_genes.tsv', sep = '\t', row.names = 1)
 
 # list of all genes in the hgsc file
-all_genes = row.names(genes_df)
+all_genes <- row.names(genes_df)
 
 # performs mapping of all genes symbols to entrezid
-mapping = select(org.Hs.eg.db, all_genes, c("ENTREZID", "GENENAME"), "SYMBOL")
+mapping <- select(org.Hs.eg.db, all_genes, c("ENTREZID", "GENENAME"), "SYMBOL")
 
 # any genes that do not initally map to an entrezid, thus the genes that have an 
 # "NA" for entrezid s
-genesNA = mapping[is.na(mapping$ENTREZID),]$SYMBOL
+genesNA <- mapping[is.na(mapping$ENTREZID),]$SYMBOL
 
 # perform second stage mapping of the genes that did not initially map with "SYMBOL"
 # option
-mappingNA = select(org.Hs.eg.db, genesNA, c("ENTREZID", "GENENAME"), "ALIAS")
+mappingNA <- select(org.Hs.eg.db, genesNA, c("ENTREZID", "GENENAME"), "ALIAS")
 
 # creates an association between the gene name and entrezid for those genes that did 
 # not map using "SYMBOL" option
-alias_lst = mappingNA$ENTREZID
-names(alias_lst) = mappingNA$ALIAS
+alias_lst <- mappingNA$ENTREZID
+names(alias_lst) <- mappingNA$ALIAS
 
 # iterates through the SYMBOL feature of the mapping object and for those genes that
 # did not map through first pass, it replaces the "NA" in enterezid with the mapped
@@ -42,7 +42,7 @@ names(alias_lst) = mappingNA$ALIAS
 for (s in 1:length(mapping$SYMBOL)){
   symbol = mapping$SYMBOL[s]
   if (is.element(symbol, names(alias_lst))){
-  mapping$ENTREZID[s] = alias_lst[symbol]
+  mapping$ENTREZID[s] <- alias_lst[symbol]
   }
 }
 
@@ -50,7 +50,7 @@ for (s in 1:length(mapping$SYMBOL)){
 mapping [is.na(mapping$ENTREZID),]$ENTREZID
 
 # sets the rownames of orignal genes_df to the new complete entrezid mapping 
-row.names(genes_df) = mapping$ENTREZID
+row.names(genes_df) <- mapping$ENTREZID
 
 # writes genes_df to tsv file
 write.table(genes_df, 'entrezid_hgsc.txt', sep = '\t')
