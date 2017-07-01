9d1e4dc8265bdfa979c7653f218a556291dfbf5c
Lia <lia@lias-work-imac.wireless.dartmouth.edu>
2017-07-01 15:02:18 -0400

    will run in terminal from start to end now

--- Scripts/gea_pipeline.sh
+++ Scripts/gea_pipeline.sh
@@ -3,31 +3,24 @@
 # Example of GEA community detection simulation and application to HGSC data
 set -o errexit
 
-### -------- Parameters
-iterations = 100
-num_paths_min = 2
-num_paths_max = 8
-percent_min = .3
-percent_max = 1
-addit_min = .1
-addit_max = 1
-file_name = 'all_iterations.csv'
-
 ### -------- Simulation pipeline
 
-# Prepare ontology and variable files
-python --Scripts/Ontology_Prep.py
+# Prepare KEGG ontology
+python Scripts/ontology_prep.py c2.cp.kegg.v5.1.entrez.gmt.txt KEGG
 
 # Run simulations
-python --Scripts/Enrichment_Main.py data_generation $iterations $num_paths_min $num_paths_max $percent_min $percent_max $addit_min $addit_max $file_name
+python Scripts/enrichment_main.py 100 2 8 .3 1 .1 1 all_iterations_data.csv 
 
 # Visualize results
 Rscript Scripts/gea_paper_figures.R
 
 ### -------- HGSC pipeline
 
+# Prepare PID ontology 
+python Scripts/ontology_prep.py PID.Entrez.DB.txt PID
+
 # Convert from Gene Symbols to Entrezid
-Rscript --convert_gene_id.R
+Rscript Scripts/convert_gene_id.R
 
 # Run HGSC analysis
-python --hgsc_analysis.py
+python Scripts/hgcs_analysis.py
