d6d8812217358bcba20d5108b20901af480242c4
Lia <lia@lias-work-imac.wireless.dartmouth.edu>
2017-07-01 14:28:31 -0400

    updated to run in terminal

--- Scripts/Ontology_Prep.py
+++ Scripts/Ontology_Prep.py
@@ -23,71 +23,93 @@ PATH_NAMES and PATH_GENES are created global variables.
 """
 import pandas as pd
 import pickle 
+import sys
 
-#Initiate GLOBAL VARIABLES
+def prepare_ontology(ontology_filename, ontology): 
+    '''
+    Description
+    Prepares ontology pathway names and genes list and saves outputs as 
+    pickle files 
 
-PATH_NAMES = []
-PATH_GENES = []
+    Arguments
+    :ontology_filenamet: name of ontology file of interest
+        options include: 
+            -c2.cp.kegg.v5.1.entrez.gmt.txt
+            -PID.Entrez.DB.txt
+    :ontology: string name of ontology of interest
+        options include: 
+            -KEGG
+            -PID
 
-# Note ALL_GENES is also a global variable
-# ---------------------------------------------------------------------------
+    Output
+    :pickle files of PATH_GENES (dictionary of genes for ontology by path number), 
+    ALL_GENES (list of all genes in the ontolgoy), PATH_NAMES (list of names of 
+    each ontology pathway), and IMP_GENENS (list of all genes in IMP)
+    '''
 
-# Choose ontology
-REACTOME = 'c2.cp.reactome.v5.1.entrez.gmt.txt'
-KEGG = 'c2.cp.kegg.v5.1.entrez.gmt.txt'
-PID = 'PID.Entrez.DB.txt'
+    #Initiate GLOBAL VARIABLES
+    
+    PATH_NAMES = []
+    PATH_GENES = []
+    
+    # Note ALL_GENES is also a global variable
+    # ---------------------------------------------------------------------------
+    
 
-FILENAME = PID
-file_ontology_name = 'PID'
-
-try:
-    FHAND = open(FILENAME)
-
-    for line in FHAND:
-        line = line.rstrip().split('\t')
-        PATH_NAMES.append(line[0])
-        PATH_GENES.append(line[2:len(line)])
-    FHAND.close()
-except:
-    print 'File not found.'
-
-# ---------------------------------------------------------------------------
-# Read in full IMP network and determine set difference between IMP and KEGG
-# and set new path_genes to exclude those genes.
-# ---------------------------------------------------------------------------
-
-try:
-    # read in all IMP genes
-    EDGE_LST = pd.read_table('global_average.filtered.txt', header=None)
-
-    # set of unique IMP genes
-    IMP_GENES = set(EDGE_LST[0]).union(set(EDGE_LST[1]))
-
-    # convert IMP gene from int to string
-    IMP_GENES = set([str(gene) for gene in IMP_GENES])
-
-    # unlists genes
-    ALL_GENES = set([genes for path in PATH_GENES for genes in path])
-
-    # finds genes in ontology not in IMP
-    DIFF = list(ALL_GENES.difference(IMP_GENES))
-
-    # creates gene ontology wo/o genes in ontology  but not in IMP
-    PATH_GENES2 = []
-    for path in PATH_GENES:
-        path = [gene for gene in path if gene not in DIFF]
-        PATH_GENES2.append(path)
-
-    PATH_GENES = PATH_GENES2
-
-    # This version removes any genes in IMP but not in ontology
-    # unlists genes
-    ALL_GENES = set([genes for path in PATH_GENES for genes in path])
-
-except:
-    print 'Invalid edege-list.'
-
-pickle.dump(PATH_GENES, open('{0}_path_genes.pkl'.format(file_ontology_name), 'w'))
-pickle.dump(ALL_GENES, open('{0}_all_genes.pkl'.format(file_ontology_name), 'w'))
-pickle.dump(PATH_NAMES, open('{0}_path_names.pkl'.format(file_ontology_name), 'w'))
-pickle.dump(IMP_GENES, open('IMP_genes.pkl', 'w'))
+    
+    try:
+        FHAND = open('./Data/{0}'.format(ontology_filename))
+    
+        for line in FHAND:
+            line = line.rstrip().split('\t')
+            PATH_NAMES.append(line[0])
+            PATH_GENES.append(line[2:len(line)])
+        FHAND.close()
+    except:
+        print 'File not found.'
+    
+    # ---------------------------------------------------------------------------
+    # Read in full IMP network and determine set difference between IMP and KEGG
+    # and set new path_genes to exclude those genes.
+    # ---------------------------------------------------------------------------
+    
+    try:
+        # read in all IMP genes
+        EDGE_LST = pd.read_table('./Data/global_average.filtered.txt', header=None)
+    
+        # set of unique IMP genes
+        IMP_GENES = set(EDGE_LST[0]).union(set(EDGE_LST[1]))
+    
+        # convert IMP gene from int to string
+        IMP_GENES = set([str(gene) for gene in IMP_GENES])
+    
+        # unlists genes
+        ALL_GENES = set([genes for path in PATH_GENES for genes in path])
+    
+        # finds genes in ontology not in IMP
+        DIFF = list(ALL_GENES.difference(IMP_GENES))
+    
+        # creates gene ontology wo/o genes in ontology  but not in IMP
+        PATH_GENES2 = []
+        for path in PATH_GENES:
+            path = [gene for gene in path if gene not in DIFF]
+            PATH_GENES2.append(path)
+    
+        PATH_GENES = PATH_GENES2
+    
+        # This version removes any genes in IMP but not in ontology
+        # unlists genes
+        ALL_GENES = set([genes for path in PATH_GENES for genes in path])
+    
+    except:
+        print 'Invalid edege-list.'
+    
+    pickle.dump(PATH_GENES, open('./Data/{0}_path_genes.pkl'.format(ontology), 'w'))
+    pickle.dump(ALL_GENES, open('./Data/{0}_all_genes.pkl'.format(ontology), 'w'))
+    pickle.dump(PATH_NAMES, open('./Data/{0}_path_names.pkl'.format(ontology), 'w'))
+    pickle.dump(IMP_GENES, open('./Data/IMP_genes.pkl', 'w'))
+    
+if __name__ == '__main__': 
+    ontology_filename = sys.argv[1]
+    ontology = sys.argv[2]
+    prepare_ontology(str(ontology_filename), str(ontology))
